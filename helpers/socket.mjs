9782adadd7447b0f377c1ec7e6b538e4523266d2
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2021-04-29 20:18:23 +0400

    fix

--- helpers/socket.mjs
+++ helpers/socket.mjs
@@ -2,7 +2,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 import http from 'http';
 
-const socketPort = +(process.env.PORT + 1) || 5001;
+const socketPort = process.env.PORT ? process.env.PORT + 1 : 5001;
 const server = http.createServer();
 const io = require('socket.io')(server);
 server.listen(socketPort, () => {
