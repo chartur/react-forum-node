d070fc26e321c7e847032f4aabc42f3a47f3f795
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2021-04-29 19:06:14 +0400

    mongo db

--- helpers/con.mjs
+++ helpers/con.mjs
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 export default (appListener) => {
+  const url = 'mongodb+srv://mern-app-db:SqcHvfEb-si5JfG@cluster0.yr0zs.mongodb.net/react?retryWrites=true&w=majority'
+  // const url = 'mongodb://localhost:27017/react'
   mongoose.connect('mongodb://localhost:27017/react', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
