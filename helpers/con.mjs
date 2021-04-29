2b63920bcf632c8b221ef669637f5e09c6b4ade8
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2021-04-29 19:54:24 +0400

    bug fix

--- helpers/con.mjs
+++ helpers/con.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export default (appListener) => {
   const url = 'mongodb+srv://mern-app-db:SqcHvfEb-si5JfG@cluster0.yr0zs.mongodb.net/react?retryWrites=true&w=majority'
   // const url = 'mongodb://localhost:27017/react'
-  mongoose.connect('mongodb://localhost:27017/react', {
+  mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
